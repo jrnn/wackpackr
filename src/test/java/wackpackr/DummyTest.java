83bdc4f18034bdcf13eba24fe096bebf230777ac
Juurinen Juho T <juho.juurinen@gmail.com>
2018-07-24 22:31:08 +0300

    hello travis

---
+++ src/test/java/wackpackr/DummyTest.java
@@ -0,0 +1,13 @@
+package wackpackr;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class DummyTest
+{
+    @Test
+    public void dummy()
+    {
+        assertEquals(2, 1 + 1);
+    }
+}
